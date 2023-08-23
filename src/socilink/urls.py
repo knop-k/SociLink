37f7ff1682d48753745b2eb101d4b7a41dc9ba7d
knop-k <knop.krzysiek@gmail.com>
2023-08-23 23:28:31 +0200

    add post_comment_create_and_list_view and like_unlike_post views

--- src/socilink/urls.py
+++ src/socilink/urls.py
@@ -23,7 +23,8 @@ from .views import home_view
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home_view, name='home-view'),
-    path('profiles/', include('apps.profiles.urls', namespace='profiles'))
+    path('profiles/', include('apps.profiles.urls', namespace='profiles')),
+    path('posts/', include('apps.posts.urls', namespace='posts'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
